0918e1bbeb94595e91912e8fa9d32094f895dd0d
Longcao7396 <80570281+Longcao7396@users.noreply.github.com>
2025-11-22 11:14:14 +0700

    update

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,16 +1,7 @@
 module com.example.quanlytoanhanhom4 {
     requires javafx.controls;
     requires javafx.fxml;
-    requires javafx.web;
     requires java.sql;
-
-    requires org.controlsfx.controls;
-    requires com.dlsc.formsfx;
-    requires net.synedra.validatorfx;
-    requires org.kordamp.ikonli.javafx;
-    requires org.kordamp.bootstrapfx.core;
-    requires eu.hansolo.tilesfx;
-    requires com.almasb.fxgl.all;
 
     opens com.example.quanlytoanhanhom4.controller to javafx.fxml;
     opens com.example.quanlytoanhanhom4.controller.auth to javafx.fxml;
